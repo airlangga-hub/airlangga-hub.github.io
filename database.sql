a48cc2513950bdb953be15cbf8d7b35e6fdc9fbd
Airlangga <airlanggakrishna@gmail.com>
2026-01-08 10:29:49 +0700

    sql

---
+++ database.sql
@@ -0,0 +1,28 @@
+-- DDL
+CREATE DATABASE IF NOT EXISTS airlangga_db;
+
+USE airlangga_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- register
+INSERT INTO users
+(first_name, last_name, email, password)
+VALUES
+('John', 'Doe', 'john@gmail.com', 'password12345');
+
+-- login
+SELECT
+    id,
+    first_name,
+    last_name,
+    password
+FROM users
+WHERE
+    email = ?;
